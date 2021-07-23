9deeaca62a14349381574114e61278852ff53891
sn <nikolasalon@gmail.com>
2021-07-23 14:33:17 +0300

    created some structure of proj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import BikeList from "./component/bikeList";
+import Editor from "./component/editor";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <BikeList />
+            <Editor />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/component/bikeList/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Bike from "../bike";
+
+export default function BikeList() {
+    return (
+        <ul>
+            <Bike />
+        </ul>
+    )
+}
